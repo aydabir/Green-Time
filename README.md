1556000bd2a9e60da474b88738e7f3bd67cf0e3e
Halil Emre ÖZEN <halilemreozen@anadolu.edu.tr>
2016-06-23 00:56:52 +0300

    First, commit with bunch of love
    Just structure and gulp file but hey it's the first commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Green-Time
-A browser plugin to help people better use their time. It aims to use a kinder approach by not blocking the pages, but just reminding to user when they visit the pages. So the user gives the final decision of leaving a page, so they are not faced with an external force.
+A browser plugin to help people better use their time. It aims to use a kinder approach by not blocking the pages, but just reminding to the user when they visit the pages. So the user gives the final decision of leaving a page, so they are not faced with an external force.
 
 ##Getting Started
 
@@ -7,38 +7,37 @@ A browser plugin to help people better use their time. It aims to use a kinder a
 
 1. Clone the repository
 2. Run your favorite command line
-3. Enter the ~/Green-Time/dev/ folder
-  1. Run " npm update " command,
-  2. Run " bower update " command,
-  3. Run " gulp " command,
-  4. Now you have ~/Green-Time/product folder.
-4. Go to **chrome://extensions/** link on your browser and click the **load unpacked extension** button
+3. Enter the ~/Green-Time/development/ folder
+  1. Run " npm install " command,
+  2. Run " gulp " command,
+  3. Now you have ~/Green-Time/product folder.
+4. Go to **chrome://extensions/** link on your browser and check **developer mode** option at top of the page then click the **load unpacked extension** button
   1. You can set ~/Green-Time/product/ folder for chrome extension package folder
-  2. Note : **[Extensions Reloader](https://chrome.google.com/webstore/detail/extensions-reloader/fimgfedafeadlieiabdeeaodndnlbhid/)** extension very useful for development workflow. Every code changes need to reload for test on chrome
+  2. Note : **[Extensions Reloader](https://chrome.google.com/webstore/detail/extensions-reloader/fimgfedafeadlieiabdeeaodndnlbhid/)** extension very useful for development workflow. Every code changes need to reload for test on chrome and the reloading easier with this extension
 5. Now you ready for contributing.
 
-Please feel free for contact us.
+Please feel free for contacting us.
 
 ##ToDo-List
 
 
 - [ ] Show notification modal
-- [x] URL-List control
+- [] URL-List control
 - [ ] Time tracking
   - [ ] Periodic Notifications
 - [ ] Plug-in settings UI
 - [ ] Activity time customization
   - [ ] Daytime
   - [ ] Weekdays
-- [x] URL-List Customization
+- [] URL-List Customization
   - [x] Single list
   - [ ] Multi list
 - [ ] Modal Customization
   - [ ] OK-button
   - [ ] Continue/Cancel
   - [ ] Auto-close
-- [x] Message Customization
-  - [x] Text
+- [] Message Customization
+  - [] Text
   - [ ] Multi-media
 - [ ] Reaction Customization
   - [ ] Close tab
---
+++ development/gulpfile.js
@@ -0,0 +1,104 @@
+"use strict";
+
+// Import 3rd party packages
+const gulp = require('gulp');
+const changed = require('gulp-changed');
+const gpath = require('gulp-path');
+const gutil = require('gulp-util');
+const count = require('gulp-count');
+const gulpif = require('gulp-if');
+
+// Gulp default settings
+const settings = {
+    // firstBuild : When gulp started call build function.
+    // -Default : true
+  firstBuild : true,
+    // autoDestToProduct : When a file changed (e.g. : css,js,html,...) build and dest to the product directory.
+    // -Default : true
+  autoDestToProduct : true
+};
+
+
+// Plugin dir structure and file declarations
+const pluginBase = new gpath.Base({
+        src: './',
+        dest: '../product/'
+});
+
+const paths = {
+    views       : pluginBase.Path('views','.html'),
+    scripts     : pluginBase.Path('scripts','.js'),
+    styles      : pluginBase.Path('styles','.css'),
+    resources   : pluginBase.Path('resources'),
+    images      : pluginBase.Path('resources/images'),
+    settings    : pluginBase.Path('resources/settings')
+};
+
+// Solution for double dot and double backslash problem of gulp-path package with self called function
+(function(){
+    for(let path in paths){
+        paths[path].src  = '.\\' + paths[path].src.toString().replace('..','.').replace(/\\\\/g, '\\');
+        paths[path].dest = paths[path].dest.toString().replace(/\\\\/g, '\\');
+    }
+})();
+
+/* Build bundle  */
+gulp.task('build',["build_scripts","build_styles","build_resource"]);
+
+gulp.task('build_scripts', () => {
+    let scripts = gulp.src(paths.scripts.src)
+	    .pipe(changed(paths.scripts.dest))
+        .pipe(gulpif(settings.autoDestToProduct, gulp.dest(paths.scripts.dest) ))
+        .pipe(gulpif(settings.autoDestToProduct, count('<%= counter %> scripts build & distributed') ));
+    return scripts;
+});
+
+/*
+* Build_styles : get sass files and compile them to css then dest to product if auto dest is available
+*/
+gulp.task('build_styles', () => {
+	let styles = gulp.src(paths.styles.src)
+		.pipe(changed(paths.styles.dest))
+		.pipe(gulpif(settings.autoDestToProduct, gulp.dest(paths.styles.dest) ))
+		.pipe(gulpif(settings.autoDestToProduct, count('<%= counter %> styles build & distributed') ));
+	return styles;
+});
+
+/*
+ * Build_resource : dest resources files
+ */
+gulp.task('build_resource', () => {
+	let views = gulp.src(paths.views.src)
+		.pipe(changed(paths.views.dest))
+		.pipe(gulpif(settings.autoDestToProduct, gulp.dest(paths.views.dest) ))
+		.pipe(gulpif(settings.autoDestToProduct, count('<%= counter %> views distributed') ));
+
+
+	let images = gulp.src(paths.images.src)
+		.pipe(changed(paths.images.dest))
+		.pipe(gulpif(settings.autoDestToProduct, gulp.dest(paths.images.dest) ))
+		.pipe(gulpif(settings.autoDestToProduct, count('<%= counter %> images distributed') ));
+
+	let productRoot = pluginBase.Path().dest.toString().replace(/\\\\/g, '\\');
+	let manifest = gulp.src(paths.settings.src)
+		.pipe(changed(productRoot))
+		.pipe(gulpif(settings.autoDestToProduct, gulp.dest(productRoot) ))
+		.pipe(gulpif(settings.autoDestToProduct, count('manifest updated') ));
+});
+
+// Watch all paths for file changes
+gulp.task('track-changes', () => {
+    gulp.watch(paths.scripts.src,['build_scripts']);
+    gulp.watch(paths.styles.src,['build_styles']);
+    gulp.watch(paths.resources.src,['build_resource']);
+});
+
+// First Build of new session
+gulp.task('first-build', () => {
+    if(settings.firstBuild){
+        gulp.start('build');
+    }
+});
+
+// Start gulp first build and assign watchers
+gulp.task('default',['first-build','track-changes']);
