9d926242573f03a219f38da590cd021777570c15
Gökhan Solak <gknsolak@gmail.com>
2016-06-26 12:57:44 +0300

    Background control for green-pass
    + Manifest is fixed.
    + Added control in background.js to filter page and display green-pass.html.
    + Green-pass.js is started from background script as a content script
      - Because, script can't be called from html, chrome doesn't permit that
      - It doesn't work properly yet. Gives error due to "tab closed" .

---
+++ development/scripts/extend/background.js
@@ -0,0 +1,42 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  console.log("onUpdated "+tab.url);
+  // check if the page should be filtered
+  doFilter = filterTab(tab);
+  // show green-pass if it does
+  if(doFilter){
+    bringGreenPass(tab)
+  }
+});
+
+chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+  console.log("onCreated "+tab.url);
+  // check if the page should be filtered
+  doFilter = filterTab(tab);
+  // show green-pass if it does
+  if(doFilter){
+    bringGreenPass(tab)
+  }
+});
+
+
+function filterTab(tab){
+  var tabURL = tab.url;
+  var n = tabURL.search("facebook.com");
+
+  if (n >= 0){
+    return true;
+  }
+
+  return false;
+}
+
+
+var fcnHandleGreenPass = function handleGreenPass(tab){
+  // call green-pass javascript
+  chrome.tabs.executeScript(null, {file: "./scripts/extend/green-pass.js"});
+}
+
+function bringGreenPass(tab){
+  // Show the green-pass view
+  chrome.tabs.update(tab.tabId, {url: "./views/green-pass.html"}, fcnHandleGreenPass);
+}
--- development/scripts/extend/green-pass.js
+++ development/scripts/extend/green-pass.js
@@ -1,7 +1,12 @@
+debugger;
+console.log("Green-pass is up and running");
 
-function leaveTab(){
+var fcnLeaveTab = function leaveTab(){
+  console.log("green-pass leaveTab");
   chrome.tabs.query({'active': true}, function(tabs) {
     // Close current active tab
     chrome.tabs.remove(tabs[0].id);
   });
 }
+
+document.getElementById("btnLeave").addEventListener("click", fcnLeaveTab);
