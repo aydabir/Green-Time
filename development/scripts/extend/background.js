9ff128b7bf29c7a108a9cece714af06a559173c5
Gökhan Solak <gknsolak@gmail.com>
2016-06-26 22:03:48 +0300

    Added waiting after green-pass visit.

--- development/scripts/extend/background.js
+++ development/scripts/extend/background.js
@@ -1,3 +1,9 @@
+// Properties of background script
+var isWaiting = false; // waiting now? control variable
+var waitTime = 5*60*1000; // bekleme süresi: 5 dakika
+// TODO: Customizable waiting time
+
+// Funcitons of background script
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   console.log("onUpdated "+tab.url);
   // check if the page should be filtered
@@ -17,11 +23,25 @@ chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
   }
 });
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("Message received: " + request.topic);
+    if (request.topic == "start waiting"){
+      startWaiting();
+    }
+
+});
+
 
 function filterTab(tab){
+  // check undefined (new tab situation)
   if(!tab || !tab.url){
     return false;
   }
+  // check waiting time
+  if(isWaiting){
+    return false;
+  }
+
   var tabURL = tab.url;
   var n = tabURL.search("facebook.com");
 
@@ -32,7 +52,6 @@ function filterTab(tab){
   return false;
 }
 
-
 var fcnHandleGreenPass = function handleGreenPass(tab){
   // call green-pass javascript
   // chrome.tabs.executeScript(null, {file: "./scripts/extend/green-pass.js"});
@@ -41,4 +60,16 @@ var fcnHandleGreenPass = function handleGreenPass(tab){
 function bringGreenPass(tab){
   // Show the green-pass view
   chrome.tabs.update(tab.tabId, {url: "./views/green-pass.html"}, fcnHandleGreenPass);
+}
+
+function startWaiting(){
+  console.log("Waiting has started");
+  isWaiting = true;
+  // start timer for waiting
+  setTimeout(endWaiting, waitTime);
+}
+
+function endWaiting(){
+  console.log("Waiting has ended");
+  isWaiting = false;
 }
--- development/scripts/extend/green-pass.js
+++ development/scripts/extend/green-pass.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var leaveBtn = document.getElementById('btnLeave');
     leaveBtn.addEventListener('click', function() {
         window.close();
-  });
+    });
 	// Visit Button
 	var visitBtn = document.getElementById('btnVisit');
     visitBtn.addEventListener('click', function() {
         window.location="http://www.facebook.com"
-
+				// If the user decides to visit anyway, background should be informed
+				// to wait a while until user will be asked again
+				chrome.runtime.sendMessage({topic: "start waiting"});
     });
 });
