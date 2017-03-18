4407af5b0fe9053ae1c8abe2a30d2945d0cbdfc1
Halil Emre ÖZEN <halilemreozen@anadolu.edu.tr>
2017-03-18 18:44:30 +0300

    after a year i dont know wtf is this

--- development/scripts/core/background.js
+++ development/scripts/core/background.js
@@ -5,13 +5,195 @@
 // It basically is a substructure
 // to call functions of extend/background.js script
 
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.onCreated.addListener(tabCreate);
-  
-  chrome.tabs.onUpdated.addListener(tabUpdate);
-  
-  chrome.runtime.onMessage.addListener(handleMessage);
-  
-  initialize();
-  
-});
+/*
+ BlockList : blueprint for the BlockList
+ @return void
+ */
+var BlockList = function () {
+  "use strict";
+
+  // Private Variables [1]
+  var _list = [];
+
+  // Private Methods [3]
+  /*
+   * loadStorage : load stored data from chrome api and set to the _list
+   * @return filled or empty array
+   */
+  var loadStorage = function () {
+    chrome.storage.sync.get("blockList", function (data) {
+      if (Util.isEmpty(data)) {
+        console.warn("Null blocked domain list");
+        return [];
+      } else {
+        _list = data.blockList;
+        return {
+          "list": data.blockList
+        };
+      }
+    });
+  };
+  var constructor = (function () {
+    loadStorage();
+  })();
+  /*
+   * find : it's return index of the founded domain if it's fail will be return false
+   * @param domain : it must be instance of Domain
+   */
+  var find = function (domain) {
+    "use strict";
+    if (!(domain instanceof Domain)) {
+      console.error("Domain is not valid");
+      return false;
+    }
+    var _index = _.findIndex(_list, {url: domain.url});
+    var result = {index: _index, status: parseInt(_index) !== parseInt(-1) ? true : false};
+    return result;
+  };
+
+  //Public Variables
+
+  // Public Methods [4]
+  /*
+   * getList : it's return the _list
+   * @return empty array or the _list
+   */
+  this.getList = function () {
+    if (Util.isEmpty(_list)) {
+      console.warn("Null blocked domain list");
+      return [];
+    } else {
+      return _list;
+    }
+  };
+  /*
+   addaddDomain : it's add a new domain to the _list
+   @param domain : must be instance of the Domain object
+   @return boolean
+   */
+  this.addDomain = function (domain) {
+    if (!(domain instanceof Domain)) {
+      console.error("Domain is not valid");
+      return false;
+    }
+    if (find(domain).status) {
+      console.error("this domain already added if you want update a domain, call the .update(domain) function");
+      return false;
+    }
+    var tempList;
+    if (_list.length > 0) {
+      tempList = this.getList();
+    } else {
+      tempList = [];
+    }
+    tempList.push(domain);
+    chrome.storage.sync.set({"blockList": tempList}, function () {
+      _list.push(domain);
+      loadStorage();
+      console.info("Added");
+    });
+    return true;
+  };
+  /*
+   updateDomain : it's update given domain from the _list
+   @param domain : must be instance of the Domain object
+   @return boolean
+   */
+  this.updateDomain = function (domain) {
+    if (!(domain instanceof Domain)) {
+      console.error("Domain is not valid");
+      return false;
+    }
+
+    var indexOfDomain = find(domain);
+    if (!indexOfDomain.status) {
+      console.error("given domain doesn't exist on the _list");
+      return false;
+    }
+    _list[indexOfDomain.index] = domain;
+
+    chrome.storage.sync.set({"blockList": _list}, function () {
+      console.info("Updated");
+    });
+    return true;
+  };
+  /*
+   view : it's print a fancy table of the _list on the console
+   @return void
+   */
+  this.view = function () {
+    console.table(blockList.getList());
+  };
+  /*
+   clear : clear all domains in _list
+   @param areYouDrunk : you must provide you're not drunk
+   @return boolean
+   */
+  this.clear = function (areYouDrunk) {
+    if (!Util.isEmpty(areYouDrunk) && areYouDrunk.toString() == "Nope") {
+      _list = [];
+      chrome.storage.sync.set({"blockList": _list}, function () {
+        console.info("Cleaned");
+      });
+      return true;
+    } else {
+      console.error("System.Exception.TooMuchBeer");
+      console.warn("Please shut the PC and go to the bed.");
+      return false;
+    }
+  }
+
+};
+
+/*
+ * Util : some helper functions
+ */
+var Util = {
+  /*
+   isEmpty : check the given object is empty,null or undefined
+   @return boolean
+   */
+  isEmpty: function (value) {
+    if (typeof value == "string") value = value.trim();
+    return value == null ||
+        value === '' ||
+        value === undefined ||
+        value === null ||
+        Object.keys(value).length === 0;
+  }
+};
+
+/*
+ Domain : blueprint of domains
+ @param paramUrl : the web url of the website
+ @return boolean only if false
+ */
+var Domain = function (_url, _waitTime, _category) {
+  "use strict";
+  if (typeof _url === "string") {
+
+    if (Util.isEmpty(_url)) {
+      throw Error("Domain url must be entered");
+    }
+
+    this.url = _url;
+    this.waitTime = _waitTime || 5;
+    this.category = _category || 'General';
+
+  } else if (typeof _url === "object") {
+
+    if (Util.isEmpty(_url.url)) {
+      throw Error("Domain url must be entered");
+    }
+
+    var obj = _url;
+    this.url = obj.url;
+    this.waitTime = obj.waitTime || 5;
+    this.category = obj.category || 'General';
+  }
+
+  this.statistics = {
+    totalVisitCount: 0,
+    totalVisitTime: 0
+  }
+};
--- development/scripts/extend/background.js
+++ development/scripts/extend/background.js
@@ -164,3 +164,24 @@ function printLog(strLog){
 
   console.log(strLog);
 }
+
+
+var plugin = {
+
+  /*
+   * onUpdate
+   * Each request pass here on load stage
+   */
+  onLoad: function(context) {
+    log('onLoad event is fired : ' + context.tab.url, 'warn');
+  },
+
+  /*
+   * beforeEnter
+   * Each request pass here on complated stage
+   */
+  onComplated: function(context) {
+    log('onComplated event is fired : ' + context.tab.url, 'warn');
+  }
+
+};
